5e4d281573fb7ce22bcd9938ae25b7d71ac6f721
Fernando Gerfauo <87751640+nandogerfauoo@users.noreply.github.com>
2022-07-14 11:36:01 -0300

    Delete backend directory

--- backend/src/main/java/com/itsurvivors/backend/Model/RoleName.java
+++
@@ -1,8 +0,0 @@
-package com.itsurvivors.backend.Model;
-
-public enum RoleName {
-    
-    USER,
-    ADMIN
-    
-}
--- backend/src/main/java/com/itsurvivors/backend/Services/IRoleService.java
+++
@@ -1,6 +0,0 @@
-package com.itsurvivors.backend.Services;
-
-public interface IRoleService<T> extends IService<T> {
-
-	T findByName(String name);
-}
