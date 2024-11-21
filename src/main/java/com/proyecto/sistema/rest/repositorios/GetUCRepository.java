ac227c02c1589d89d29ad4a98740ad5a5fc58fc0
federico.lopez <federico.lopez@estudiantes.utec.edu.uy>
2024-11-20 21:25:59 -0300

    Revert "Se agregan ultimas clases"
    This reverts commit c8605d83100d16c504dfad27555eebb3b4b59914.

--- src/main/java/com/proyecto/sistema/rest/repositorios/GetUCRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.sistema.rest.repositorios;
-
-import com.proyecto.sistema.clases.sistema.UnidadesCurriculares;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GetUCRepository  extends JpaRepository<UnidadesCurriculares, Long> {
-    // Método para encontrar un usuario por nombre de usuario y contraseña
-}
-
